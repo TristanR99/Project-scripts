abe6ff65a46f83294cf274f1ec2ba8bd3ef4225a
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2020-04-20 16:51:58 -0400

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerMovement : MonoBehaviourPun
+{
+  public CharacterController controller;
+
+  public float speed = 12f;
+  public float gravity = -9.81f;
+
+  public Transform groundCheck;
+  public float groundDistance = 0.4f;
+  public LayerMask groundMask;
+
+  Vector3 velocity;
+  bool isGrounded;
+
+  void Update()
+  {
+    if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+    {
+      return;
+    }
+    if (Cursor.lockState == CursorLockMode.Locked)
+    {
+      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+      if (isGrounded && velocity.y < 0)
+        velocity.y = -2f;
+
+      float x = Input.GetAxis("Horizontal");
+      float z = Input.GetAxis("Vertical");
+
+      Vector3 move = transform.right * x + transform.forward * z;
+
+      controller.Move(move * speed * Time.deltaTime);
+
+      velocity.y += gravity * Time.deltaTime;
+
+      controller.Move(velocity * Time.deltaTime);
+    }
+  }
+}
